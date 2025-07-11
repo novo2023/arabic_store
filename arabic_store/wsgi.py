8bbef6dc9b53e9f16f19ae82e1917875743637b7
novo <akhmad.zealand@gmail.com>
2025-07-11 15:24:14 +0700

    Fix: update wsgi to use correct module name

--- arabic_store/wsgi.py
+++ arabic_store/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arabicstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arabic_store.settings')
 
 application = get_wsgi_application()
