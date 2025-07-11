111f004b0cdf34bded186946920f3b3a1eb87107
novo <akhmad.zealand@gmail.com>
2025-07-12 03:13:22 +0700

    Update settings.py for Render database

--- arabic_store/settings.py
+++ arabic_store/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'arabic_store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'arabicstore',
-        'USER': 'postgres',
-        'PASSWORD': 'nono',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME', 'arabicstore'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'nono'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
